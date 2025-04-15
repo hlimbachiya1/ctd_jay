d0acb392f38c39b3472ac0bb72924c3095d1b0c6
Hemang Limbachiya <hlimbachiya@hawk.iit.edu>
2025-04-15 16:46:07 -0500

    Implement conditional rendering and controlled components

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,27 @@ function App() {
     const [todoList, setTodoList] = useState([]);
 
     const handleAddTodo = (newTodo) => {
-	setTodoList([...todoList, newTodo]);
+        const todoWithCompletion = {...newTodo, isCompleted:false};
+	setTodoList([...todoList, todoWithCompletion]);
     };
+
+    const completeTodo = (id) => {
+        const updatedTodos = todoList.map(todo => {
+            if (todo.id === id) 
+                {return {...todo, isCompleted: true}; 
+            }
+            return todo;
+        });
+        setTodoList(updatedTodos);
+    }
 
     return (
         <div>
             <h1> My Todos</h1>
             <TodoForm onAddTodo={handleAddTodo} />  {/* Pass the function */}
             
-            <TodoList todoList={todoList}/>  {/* Pass todoList as prop */}
+            <TodoList todoList={todoList} 
+                      onCompleteTodo={completeTodo}/>  {/* Pass todoList as prop */}
         </div>
     ) 
 }  
